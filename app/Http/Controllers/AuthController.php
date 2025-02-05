96c6e930d3743b0e90076dbbba0bf1c02de2acc1
Rakentod <Rakaaditya1391@gmail.com>
2025-02-05 14:09:08 +0700

    p

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,12 +1,19 @@
 <?php
 
 namespace App\Http\Controllers;
-
+ 
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+    function profile(request $request){
+        return Auth::user();
+    }
+
+
     function register(request $request){
         $request->validate([
             'name' => 'required|string|min:2|max:58',
@@ -22,5 +29,28 @@ class AuthController extends Controller
             return response([
                 "message" => 'User registered successfully'
             ], 201);
+    }
+
+    function login(request $request){
+        $request->validate([
+            'email' =>'required|email',
+            'password' => 'required|min:8'
+        ]);
+
+        $user = User::whereEmail($request->email)->first();
+
+        if(!$user ||!Hash::check($request->password, $user->password)){
+            return response([
+                "message" => 'Invalid pass'
+            ], 401);
+        }
+
+        $token = $user->createToken($user->name . '_AuthToken')->plainTextToken;
+
+        return response([
+            "message" => 'Logged in successfully',
+            "access_token" => $token,
+
+        ],201);
     }
 }
